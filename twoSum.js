351ff54c0cdc23692aab38f2c99c87e03a56c3d1
adhiambokonyango <marykonyango74@gmail.com>
2022-05-08 21:21:01 +0300

    twoSum

--- twoSum.js
+++ twoSum.js
@@ -2,6 +2,7 @@ module.exports = twoSum = (nums, target) => {
     console.log("incoming array")
     console.log(nums)
     let map = new Map();
+    let resultantArr = [];
 
     for (let i = 0; i<nums.length; i++){
         let compliment = target-nums[i];
@@ -9,13 +10,15 @@ module.exports = twoSum = (nums, target) => {
     }
 
     for (let i = 0; i<nums.length; i++){
-            console.log("elements found")
-            console.log(map.get(nums[i]))
+        if (map.get(nums[i]) !== undefined){
+            resultantArr.push(map.get(nums[i]));
+        }
+
     }
-
-    console.log(map.size)
+    console.log("elements found")
+    console.log(resultantArr)
+    console.log( `map_size: ${map.size}`)
 }
-
 // time complexity O(n)
 
 
